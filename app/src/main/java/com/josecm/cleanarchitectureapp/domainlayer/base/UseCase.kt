1f85d5d39b47b7054d2f72e92d45f22c18c15489
JoseFelixCastillo <josefelix.castillomoya@gmail.com>
2019-03-25 01:15:41 +0100

    view model

--- app/src/main/java/com/josecm/cleanarchitectureapp/domainlayer/base/UseCase.kt
+++ app/src/main/java/com/josecm/cleanarchitectureapp/domainlayer/base/UseCase.kt
@@ -1,5 +1,10 @@
 package com.josecm.cleanarchitectureapp.domainlayer.base
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
 /**
  * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
  * This abstraction represents an execution unit for different use cases (this means than any use
@@ -12,9 +17,9 @@ abstract class UseCase<out Type, in Params> where Type : Any {
 
     abstract suspend fun run(params: Params): Either<Failure, Type>
 
-    operator fun invoke(params: Params, onResult: (Either<Failure, Type>) -> Unit = {}) {
-        val job = async(CommonPool) { run(params) }
-        launch(UI) { onResult(job.await()) }
+    operator fun invoke(scope: CoroutineScope, params: Params, onResult: (Either<Failure, Type>) -> Unit = {}) {
+        val job = scope.async(Dispatchers.IO) { run(params) }
+        scope.launch(Dispatchers.Main) { onResult(job.await()) }
     }
 
     class None
--- app/src/main/java/com/josecm/cleanarchitectureapp/presentationlayer/MainActivityViewModel.kt
+++ app/src/main/java/com/josecm/cleanarchitectureapp/presentationlayer/MainActivityViewModel.kt
@@ -1,7 +1,42 @@
 package com.josecm.cleanarchitectureapp.presentationlayer
 
 import android.arch.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel: ViewModel(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + viewModelJob
+
+    private val viewModelJob = SupervisorJob()
+
+    private fun getService() {
+        //val job = Job
+        //laun
+
+    }
+
+    /**
+     * Cancel all coroutines when the ViewModel is cleared
+     */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    /**
+     * Heavy operation that cannot be done in the Main Thread
+     */
+    fun launchDataLoad() {
+        launch {
+            sortList()
+            // Modify UI
+        }
+    }
+
+    suspend fun sortList() = withContext(Dispatchers.Default) {
+        // Heavy work
+    }
 
 }
